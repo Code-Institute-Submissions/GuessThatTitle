20c0ec2a9bbf23ac339412a0066a5d6db3459d5d
davidosongschool <davidosongschool@gmail.com>
2020-05-06 13:19:54 +0000

    select movies from api and store in array

--- assets/js/engine.js
+++ assets/js/engine.js
@@ -2,7 +2,7 @@
 
 var roundPosition; // Track what round they are on
 var score; // Track their score
-
+var pageNumber;
 /* Change every Round*/
 
 var numberOfLives; // Store lives
@@ -10,6 +10,8 @@ var numberOfPasses; // Store number of passes left
 var currentQuestionNumber; // Store what question they are on out of 10
 var questionNumber; // Track what question they are on (out of 10)
 var topic;
+var tempQuestion = []; // Store question data temp before pushing array into quesiton array
+var questions = []; // store questions for each round 
 
 /* Change every question */
 
@@ -49,10 +51,10 @@ function getTopics() {
     }  while (x === y || y === z || z === x);
 
    // Select Topics from JSON (need to minus one since array starts at 0)
-   var topicOne = jsonData[Object.keys(jsonData)[x -1]];
+   var topicOne = jsonData[Object.keys(jsonData)[x - 1]];
    var topicTwo = jsonData[Object.keys(jsonData)[y -1]];  
    var topicThree = jsonData[Object.keys(jsonData)[z -1]];  
-   
+
    
    document.getElementById("topic1").innerHTML = topicOne; 
    document.getElementById("topic2").innerHTML = topicTwo; 
@@ -69,7 +71,7 @@ $(".choose-topic-btn").click(function(){
 topic = $(this).text();
 console.log("Topic Choosen: " + topic);
 // Now you need to generate the questions based on this topic
-generateQuestion();
+generateQuestion(); // Generate 6 questions (3 random movies from 2 random pages)
 }); 
 
 // Generate the question
@@ -77,7 +79,8 @@ function generateQuestion() {
 // Called after data is gathered
 
 function callApi(cb) {
-var URL = "https://www.omdbapi.com/?apikey=c1466e63&s=" + topic; // Set url for query to api based on topic selected
+console.log("Getting data from page : " + pageNumber);
+var URL = "https://www.omdbapi.com/?apikey=c1466e63&s=" + topic + "&page=" + pageNumber; // Set url for query to api based on topic selected
 var xhr = new XMLHttpRequest();
     // set url based on selected trending item
     xhr.open("GET", URL);
@@ -89,34 +92,70 @@ var xhr = new XMLHttpRequest();
     };
 }
 
-
-
 function processData (info) {
-    // Do stuff with data here 
-    var movies = info;
-    console.log(movies);
 
+ var movies = info;
+  // Select three random movie titles
+  var x=1;
+  var y=1;
+  var z=1;
+  
+  // Generate three different random numbers (don't want same topic selected twice)
+  do{
+        x = Math.round(Math.random() * 10);
+        y = Math.round(Math.random() * 10);
+        z = Math.round(Math.random() * 10);
+    }  while (x === y || y === z || z === x);
+
+
+    var selected = movies.Search[x - 1]; 
+    console.log(selected);
+    var movieTitle = selected[Object.keys(selected)[0]]; // Movie Title - Push to Array
+    var movieYear = selected[Object.keys(selected)[1]]; // Movie Year - Push to Array
+
+    // Create an array and then push into Questions Array
+    tempQuestion = [movieTitle, movieYear];
+    questions.push(tempQuestion);
+
+
+    var selected = movies.Search[y - 1]; 
+    console.log(selected);
+    var movieTitle = selected[Object.keys(selected)[0]]; // Movie Title - Push to Array
+    var movieYear = selected[Object.keys(selected)[1]]; // Movie Year - Push to Array
+
+    // Create an array and then push into Questions Array
+    tempQuestion = [movieTitle, movieYear];
+    questions.push(tempQuestion);
     
+    
+    var selected = movies.Search[z - 1]; 
+    console.log(selected);
+    var movieTitle = selected[Object.keys(selected)[0]]; // Movie Title - Push to Array
+    var movieYear = selected[Object.keys(selected)[1]]; // Movie Year - Push to Array
+    // Create an array and then push into Questions Array
+    tempQuestion = [movieTitle, movieYear];
+    questions.push(tempQuestion);
+}
+// Generate two random numbers     
+  var x=1;
+  var y=1;
+  do{
+        x = Math.round(Math.random() * 10);
+        y = Math.round(Math.random() * 10);
+     
+    }  while (x === y);
+
+// Get data from two random pages 
+pageNumber = x;
+for (i=0; i < 2; i++) {  
+callApi(processData); // Call on the API to get movies based on topic from two random pages 
+pageNumber = y;
+}
+// At this point you have 6 questions stored in an array with their respective years
+console.log(questions);
+
 
 }
-
-
-callApi(processData); // Call on the API to get movies based on topic
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 /* Start Game function */
